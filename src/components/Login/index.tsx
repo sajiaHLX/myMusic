67628c95b90229b41b9401b3f70b70f4af498de8
“sajiaHLX” <690906573@qq.com>
2020-11-20 17:27:27 +0800

    登陆窗口样式

--- src/components/Login/index.tsx
+++ src/components/Login/index.tsx
@@ -15,7 +15,7 @@ function ShowLoginModal() {
   let left = 0;
   let loginModal: HTMLElement | null = null;
 
-  
+
   const div = document.createElement('div');
   document.body.appendChild(div);
   render(<div>
@@ -44,14 +44,44 @@ function ShowLoginModal() {
         onMouseDown={() => {
           if (!loginModal) {
             loginModal = document.getElementById('login-modal');
+            loginModal!.style.cursor = 'move';
           }
         }}
         onMouseUp={() => {
+          loginModal!.style.cursor = '';
           loginModal = null;
         }}
       >
         <div className="set" >
           <em>登陆</em><CloseOutlined onClick={destroy} className="close-b" />
+        </div>
+      </div>
+      <div className="content">
+        <div className="l-main">
+          <div className="login-png"></div>
+          <div className="login-btn">
+            <a className="btn-1"><i className="btn-t-1">手机号登陆</i></a>
+          </div>
+          <div className="login-btn">
+            <a className="btn-2"><i className="btn-t-2">注  册</i></a>
+          </div>
+        </div>
+        <div className="l-alt">
+          <ul>
+            <li><a><i className="mlg2 mlg2-wx"></i>微信登陆</a></li>
+            <li><a><i className="mlg2 mlg2-qq"></i>QQ登陆</a></li>
+            <li><a><i className="mlg2 mlg2-wb"></i>微博登录</a></li>
+            <li><a><i className="mlg2 mlg2-wy"></i>网易邮箱账号登陆</a></li>
+          </ul>
+        </div>
+        <div className="l-bottom">
+          <div className="u-official-terms">
+            <input type="checkbox" id="j-official-terms" />
+            <label style={{ marginLeft: '2px' }}>同意</label>
+            <a href="http://st.music.163.com/official-terms/service" target="_blank" rel="noopener noreferrer"  style={{ color: '#507DAF' }}>《服务条款》</a>
+            <a href="http://st.music.163.com/official-terms/privacy" target="_blank" rel="noopener noreferrer"  style={{ color: '#507DAF' }}>《隐私政策》</a>
+            <a href="https://st.music.163.com/official-terms/children" target="_blank" rel="noopener noreferrer"  style={{ color: '#507DAF' }}>《儿童隐私政策》</a>
+          </div>
         </div>
       </div>
     </div>
